c61ed6982e2b8676c4c66105b67f690643ca62a7
Julie Brown <Julie.Brown@djoglobal.com>
2022-07-15 06:00:32 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class Human:
+
+    name = "Julie"  # name of person
+    city = "Austin"  # city of birth
+    time = "3:11PM"  # time of birth
+
+    def __init__(self, name, city, time):
+        self.name = name
+        self.city = city
+        self.time = time
